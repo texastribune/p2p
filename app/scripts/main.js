d88472723f94e8a0335b9095f1a98844e70a2ba1
jolie_sky <jolie.mccullough@gmail.com>
2015-07-28 17:57:15 -0500

    Story pages starting to look like real page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+$(window).load(function() {
+  // Menu
+  var $menuButton = $('#menu-button');
+  var $menuNav = $('#menu');
+
+  $menuButton.on('click', function(e) {
+    e.preventDefault();
+
+    if ($menuNav.hasClass('menu-open')) {
+      $menuButton.removeClass('x');
+    } else {
+      $menuButton.addClass('x');
+    }
+
+    $menuNav.toggleClass('menu-open');
+  });
+});
+
+//sets nav to sticky at certain height
+$(window).scroll(function () {
+  var navHeight = $('.masthead .menu-container').height();
+  if( $(window).scrollTop() > navHeight && !($('#menu').hasClass('sticky'))){
+    $('#menu').addClass('sticky');
+  } else if ($(window).scrollTop() < navHeight){
+    $('#menu').removeClass('sticky');
+  }
+});
