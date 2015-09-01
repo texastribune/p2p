5563f201d38d812610fb669c0080f0b963789352
jolie_sky <jolie.mccullough@gmail.com>
2015-09-01 13:25:49 -0500

    change cards to all be in one spot

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,7 +20,14 @@ $(window).load(function() {
   //lightboxes
   $('.open-popup-link').magnificPopup({
     type:'inline',
-    midClick: true
+    fixedContentPos: false,
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
   });
 });
 
